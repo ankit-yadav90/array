8c28c635c93a8055d4ba736dbe45f002f5f187a3
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-26 14:20:38 +0530

    array code string and number

---
+++ first.js
@@ -0,0 +1,6 @@
+let marks = [95 , 82 , 75 , 64 , 36];
+console.log(marks);
+console.log(marks.length);
+
+let heroes = ["ironman" , "thor" ,"hulk", "shaktiman", "spiderman", "antman"];
+console.log(heroes);
